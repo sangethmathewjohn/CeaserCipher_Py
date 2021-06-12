61f21230e2896ba893401caedf04b65f22eab4b5
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-06-12 23:04:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def ceaser(message,shifts,direction):
       encrypted += alphabet[newpos]
     else :
       encrypted += i
-  print(f"The {direction}d of {message} is: \n{encrypted}")
+  print(f"The {direction}d message is: \n{encrypted}")
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 print(art.logo+"\n")
